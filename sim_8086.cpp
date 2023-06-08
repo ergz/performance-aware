8c0f9892cdb057e3b12b1bb7d92b5900c7340904
ergz <e.emanuel.rodriguez@gmail.com>
2023-06-07 23:52:23 -0700

    fix filename

--- sim_8086.cpp
+++ sim_8086.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     FILE *file_input;
-    file_input = fopen("listing_0037_single_register_mov.asm", "rb");
+    file_input = fopen("listing_0037_single_register_mov", "rb");
 
     if (file_input == NULL) 
     {
@@ -15,7 +15,7 @@ int main()
     char input_buffer[2]; // 2 bytes of data
     fread(input_buffer, 1, 2, file_input);
 
-    
+
 
 
 
