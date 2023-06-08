3e8b9de2b89f370dd7b5581bf61e0653527bd925
ergz <e.emanuel.rodriguez@gmail.com>
2023-06-07 23:51:39 -0700

    start writting the cpp version

---
+++ sim_8086.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *file_input;
+    file_input = fopen("listing_0037_single_register_mov.asm", "rb");
+
+    if (file_input == NULL) 
+    {
+        printf("file not found\n");
+    }
+
+    printf("file found\n");
+
+    char input_buffer[2]; // 2 bytes of data
+    fread(input_buffer, 1, 2, file_input);
+
+    
+
+
+
+    fclose(file_input);
+    return(0);
+
+}
