71a3a4519d296ce8f537a4a7ab1d219cc57b9516
ergz <e.emanuel.rodriguez@gmail.com>
2023-06-08 22:37:02 -0700

    use stdint

--- sim_8086.cpp
+++ sim_8086.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main()
 {
@@ -12,44 +13,52 @@ int main()
 
     printf("file found\n");
 
-    unsigned char input_buffer[2]; // 2 bytes of data
+    uint8_t input_buffer[2]; // 2 bytes of data
     fread(input_buffer, 1, 2, file_input);
 
-    unsigned char hi_byte = input_buffer[0];
-    unsigned char lo_byte = input_buffer[1];
+    uint8_t hi_byte = input_buffer[0];
+    uint8_t lo_byte = input_buffer[1];
 
     printf("the value of the lo_byte: %d\n", lo_byte);
     printf("the value of the hi_byte: %d\n", hi_byte);
 
-    unsigned char opcode = hi_byte >> 2;
+    uint8_t opcode = hi_byte >> 2;
 
     printf("the value of the opcode is %d\n", opcode);
 
-    unsigned char d_field = hi_byte & 2;
-    unsigned char w_field = hi_byte & 1;
+    uint8_t d_field = hi_byte & 2;
+    uint8_t w_field = hi_byte & 1;
 
     printf("the value of the d_field is %d\n", d_field);
     printf("the value of the w_field is %d\n", w_field);
 
-    unsigned char mode = lo_byte >> 6;
-    unsigned char reg_field = (lo_byte >> 3) & 7;
-    unsigned char rm_field = lo_byte & 7;
+    uint8_t mode = lo_byte >> 6;
+    uint8_t reg_field = (lo_byte >> 3) & 7;
+    uint8_t rm_field = lo_byte & 7;
 
-    unsigned char asm_op;
-    unsigned char asm_d;
-    unsigned char asm_w;
-    unsigned char asm_reg;
-    unsigned char asm_rm;
+    char *asm_op;
+    // int8_t asm_d;
+    // int8_t asm_w;
+    // int8_t asm_reg;
+    // int8_t asm_rm;
 
-
-    if (opcode == "34") 
-    {
-        asm_op = "mov"
+    switch (opcode) {
+        case 34: {
+            asm_op = "mon";
+        } break;
+        default: {
+            asm_op = NULL
+        }
     }
-    else 
+
+    if (asm_op == NULL) 
     {
-        asm_op = NULL;
+        printf("opcode not implemented or does not exist\n");
+        exit(1);
     }
+
+
+
 
 
     fclose(file_input);
