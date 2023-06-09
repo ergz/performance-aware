45132776ddeb23cd2de0adaf9c0e41f5effa33ad
ergz <e.emanuel.rodriguez@gmail.com>
2023-06-08 20:48:09 -0700

    wip

--- sim_8086.cpp
+++ sim_8086.cpp
@@ -12,11 +12,44 @@ int main()
 
     printf("file found\n");
 
-    char input_buffer[2]; // 2 bytes of data
+    unsigned char input_buffer[2]; // 2 bytes of data
     fread(input_buffer, 1, 2, file_input);
 
+    unsigned char hi_byte = input_buffer[0];
+    unsigned char lo_byte = input_buffer[1];
+
+    printf("the value of the lo_byte: %d\n", lo_byte);
+    printf("the value of the hi_byte: %d\n", hi_byte);
+
+    unsigned char opcode = hi_byte >> 2;
+
+    printf("the value of the opcode is %d\n", opcode);
+
+    unsigned char d_field = hi_byte & 2;
+    unsigned char w_field = hi_byte & 1;
+
+    printf("the value of the d_field is %d\n", d_field);
+    printf("the value of the w_field is %d\n", w_field);
+
+    unsigned char mode = lo_byte >> 6;
+    unsigned char reg_field = (lo_byte >> 3) & 7;
+    unsigned char rm_field = lo_byte & 7;
+
+    unsigned char asm_op;
+    unsigned char asm_d;
+    unsigned char asm_w;
+    unsigned char asm_reg;
+    unsigned char asm_rm;
 
 
+    if (opcode == "34") 
+    {
+        asm_op = "mov"
+    }
+    else 
+    {
+        asm_op = NULL;
+    }
 
 
     fclose(file_input);
