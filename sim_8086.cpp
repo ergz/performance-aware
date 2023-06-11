b3c0e73ee982f1028e20443f34e3ec1f5e51f2ae
ergz <e.emanuel.rodriguez@gmail.com>
2023-06-11 01:39:31 -0700

    really bad implementation will improve now that it works on first case

--- sim_8086.cpp
+++ sim_8086.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-int main()
+
+int main(int argc, char *argv[])
 {
+
+    if (argc == 1) 
+    {
+        printf("no assembled file was provided to program\n");
+        exit(1);
+    }
     FILE *file_input;
     file_input = fopen("listing_0037_single_register_mov", "rb");
 
@@ -26,7 +34,12 @@ int main()
 
     printf("the value of the opcode is %d\n", opcode);
 
+    // 1: the reg field identifies the destination of op
+    // 0: the reg field identifies the source of op
     uint8_t d_field = hi_byte & 2;
+    
+    // 1: use word size
+    // 0: use byte size
     uint8_t w_field = hi_byte & 1;
 
     printf("the value of the d_field is %d\n", d_field);
@@ -39,16 +52,73 @@ int main()
     char *asm_op;
     // int8_t asm_d;
     // int8_t asm_w;
-    // int8_t asm_reg;
+    char *asm_reg;
     // int8_t asm_rm;
 
     switch (opcode) {
         case 34: {
-            asm_op = "mon";
+            asm_op = "mov";
         } break;
         default: {
-            asm_op = NULL
-        }
+            asm_op = NULL;
+        } break;
+    }
+
+    char *asm_rm;
+    switch (reg_field)
+    {
+        case 0: {
+            asm_reg = "ax";
+        } break;
+        case 1: {
+            asm_reg = "cx";
+          } break;
+        case 2: {
+            asm_reg = "dx";
+          } break;
+        case 3: {
+            asm_reg = "bx";
+          } break;
+        case 4: {
+            asm_reg = "sp";
+          } break;
+        case 5: {
+            asm_reg = "bp";
+          } break;
+        case 6: {
+            asm_reg = "si";
+        } break;
+        case 7: {
+            asm_reg = "di";
+        } break; 
+    }
+
+    switch (rm_field)
+    {
+        case 0: {
+            asm_rm = "ax";
+        } break;
+        case 1: {
+            asm_rm = "cx";
+          } break;
+        case 2: {
+            asm_rm = "dx";
+          } break;
+        case 3: {
+            asm_rm = "bx";
+          } break;
+        case 4: {
+            asm_rm = "sp";
+          } break;
+        case 5: {
+            asm_rm = "bp";
+          } break;
+        case 6: {
+            asm_rm = "si";
+        } break;
+        case 7: {
+            asm_rm = "di";
+        } break; 
     }
 
     if (asm_op == NULL) 
@@ -57,11 +127,51 @@ int main()
         exit(1);
     }
 
+    char asm_str[20];
 
 
+    if (d_field)  
+    {
+        // case when the reg field identifies the destination
+        if (w_field) 
+        {
+            // use word size 
+            printf("not implemented yet\n");
+            exit(1);            
+        }
+        else 
+        {
+            // use byte size
+            printf("not implemented yet\n");
+            exit(1);    
+        }
+    }
+    else
+    {  
+        // case when the reg field identifies the source
+        if (w_field) 
+        {
+            // use word size 
+            sprintf(asm_str, "%s %s, %s", asm_op, asm_rm, asm_reg);
+        }
+        else 
+        {
+            // use byte size
+            printf("not implemented yet\n");
+            exit(1);
+        }
 
+    }
 
     fclose(file_input);
+
+    FILE *f_out;
+    f_out = fopen("c_asm.asm", "w");
+    fprintf(f_out, asm_str);
+    fclose(f_out);
+
+
+
     return(0);
 
 }
